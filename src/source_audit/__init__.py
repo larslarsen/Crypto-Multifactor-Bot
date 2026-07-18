247787b2c4a755c3f48fe8b5e9ad4cffa2fbf99d
Lars <lars@local>
2026-07-18 08:29:52 -0700

    AUD-001: silence intentional re-export F401 [Research Sprint 003]

--- src/source_audit/__init__.py
+++ src/source_audit/__init__.py
@@ -1,32 +1,10 @@
 """Source Audit Toolkit - Research Sprint 003"""
 
-from .errors import (
-    AuditError,
-    DownloadError,
-    ChecksumMismatchError,
-    UnsafeArchiveError,
-    MalformedCSVError,
-    AmbiguousTimestampError,
-    PaginationError,
-    OrderingViolationError,
-    InvalidNumericError,
-)
-from .models import (
-    DownloadResult,
-    ZipMemberInfo,
-    ZipAuditResult,
-    CSVAuditResult,
-    TimestampInference,
-    BinancePrecisionComparison,
-    PaginationResult,
-    OHLCVBar,
-    BarReconstructionResult,
-    StorageStats,
-    AuditReport,
-)
-from .timestamps import infer_timestamp_unit
-from .archives import audit_zip_safe, audit_csv_safe
-from .pagination import paginate
-from .bars import reconstruct_bars, compare_bars
+from .errors import *  # noqa: F403
+from .models import *  # noqa: F403
+from .timestamps import infer_timestamp_unit  # noqa: F401
+from .archives import audit_zip_safe, audit_csv_safe  # noqa: F401
+from .pagination import paginate  # noqa: F401
+from .bars import reconstruct_bars, compare_bars  # noqa: F401
 
 __version__ = "0.1.0"
